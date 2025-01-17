393fa6f614155c21149392fb39c1d4865daa0235
snwrr <ylala2910@gmail.com>
2025-01-17 16:51:53 +0100

    fix conflict types.h

--- types.h
+++ types.h
@@ -7,7 +7,7 @@
 
 /**
  * représente une pièce du jeu
- * 
+ *
  * @param lettre : lettre inscrite sur la pièce
  * @param point : valeur de la pièce
  */
@@ -18,26 +18,26 @@ typedef struct
 
 } piece_t;
 
-/** 
-* représente une case du plateau
-*
-* @param valeur : 0 si c'est une case simple, 2 pour une case double et 3 pour une case triple
-* @param x : position de la case en x
-* @param y : position de la case en y
-**/
+/**
+ * représente une case du plateau
+ *
+ * @param valeur : 0 si c'est une case simple, 2 pour une case double et 3 pour une case triple
+ * @param x : position de la case en x
+ * @param y : position de la case en y
+ **/
 typedef struct
 {
     int valeurLettre;
     int valeurMot;
     int x;
     int y;
-    piece_t piece; 
+    piece_t piece;
 
 } case_t;
 
 /**
  * représente le plateau de jeu
- * 
+ *
  * @param x : largeur du plateau (nombre de colonnes)
  * @param y : longueur du plateau (nombre de lignes)
  * @param plateau : tableau en deux dimensions représentant le plateau
@@ -46,36 +46,50 @@ typedef struct
 {
     int x;
     int y;
-    case_t** plateau;
+    case_t **plateau;
 
 } plateau_t;
 
-/**
- * représente les valeurs de l'alphabet (les points pour chaque lettre)
- *
- * @param alphabet : tableau de 26 pieces représentant les valeurs des lettres de l'alphabet
- */
-typedef struct
-{
-    piece_t alphabet[26];
-} alphabet_t;
+/***********************************************************/
+/*******************GETTERS/SETTERS*PIECE*******************/
+/***********************************************************/
 
-// Déclarations des getters et setters pour piece
 char getLettre(piece_t *p);
 void setLettre(piece_t *p, char lettre);
+
 int getPoint(piece_t *p);
 void setPoint(piece_t *p, int point);
 
-// Déclarations des getters et setters pour Case
+/***********************************************************/
+/*******************GETTERS/SETTERS*PIECE*******************/
+/***********************************************************/
+
 int getValeurLettre(case_t *c);
 void setValeurLettre(case_t *c, int valeurLettre);
+
 int getValeurMot(case_t *c);
 void setValeurMot(case_t *c, int valeurMot);
+
 int getX(case_t *c);
 void setX(case_t *c, int x);
+
 int getY(case_t *c);
 void setY(case_t *c, int y);
+
 piece_t getPiece(case_t *c);
 void setPiece(case_t *c, piece_t p);
 
-#endif //TYPES_H_INCLUDED
+/***********************************************************/
+/******************GETTERS/SETTERS*TABLEAU******************/
+/***********************************************************/
+
+int getXPlateau(plateau_t *plateau);
+void setXPlateau(plateau_t *plateau, int x);
+
+int getYplateau(plateau_t *plateau);
+void setYplateau(plateau_t *plateau, int y);
+
+case_t **getPlateau(plateau_t *plateau);
+void setPlateau(plateau_t *plateau, case_t **plateau_data);
+
+#endif // TYPES_H_INCLUDED
