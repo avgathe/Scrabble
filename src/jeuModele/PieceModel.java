fd6e195f530f4bce162dc3df5935752bbc36dcb9
Agathe <vautier.agathe@gmail.com>
2025-03-19 10:57:36 +0100

    [update] PieceModel method

--- src/jeuModele/PieceModel.java
+++ src/jeuModele/PieceModel.java
@@ -9,4 +9,37 @@ public class PieceModel {
         this.letter = letter;
         this.value = value;
     }
+
+    public boolean isInAlphabet(Alphabet alphabet) {
+        return alphabet.getAlphabet().containsKey(this);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof Letter l) {
+            return l.getLetter() == this.letter;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+
+        return 31 * Character.hashCode(this.letter);
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(this.letter);
+    }
+
+
+    /**getter**/
+    public char getLetter() {
+        return letter;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
