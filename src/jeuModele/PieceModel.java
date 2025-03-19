b34abb16584c1c0b3ad3fa17e159d89157cedf42
Agathe <vautier.agathe@gmail.com>
2025-03-19 10:54:11 +0100

    [feat] PieceModel class

---
+++ src/jeuModele/PieceModel.java
@@ -0,0 +1,12 @@
+package src.gameModel;
+
+public class PieceModel {
+
+    private final char letter;
+    private final int value;
+
+    public PieceModel(char letter, int value) {
+        this.letter = letter;
+        this.value = value;
+    }
+}
